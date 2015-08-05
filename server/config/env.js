987b2c944a21ec113abcafda02c82a988a046601
horologix <tangelocat@gmail.com>
2015-08-05 12:30:07 -0700

    [initial] env config file

---
+++ server/config/env.js
@@ -0,0 +1,4 @@
+module.exports = {
+    serverIp: "127.0.0.1",
+    serverPort: "8000"
+};
--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
-var express = require("express")
-    app     = express();;
+var express = require("express");
+var app = express();
+
+var configEnv = require("./config/env.js");
 
 // --------------------------------------------------------------------------------
 
 app.use(express.static("public"));
-app.listen(3333, "localhost", function(){console.log("Server started: listening...");});
+app.listen(configEnv.serverPort, configEnv.serverIp, function(){console.log("Server started: listening...");});
