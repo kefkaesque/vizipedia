016482ae27f388ea115ebd03bcd6ce2faf529df8
horologix <tangelocat@gmail.com>
2015-08-05 14:56:25 -0700

    [initial] add routes.js

--- server/server.js
+++ server/server.js
@@ -6,4 +6,6 @@ var configEnv = require("./config/env.js");
 // --------------------------------------------------------------------------------
 
 app.use(express.static("public"));
+
+
 app.listen(configEnv.serverPort, configEnv.serverIp, function(){console.log("Server started: listening...");});
