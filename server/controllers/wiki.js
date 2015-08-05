298c8136d60010fa476b5f4124e6a987617533d0
carterchung <carterchung@gmail.com>
2015-08-05 16:52:44 -0700

    [feat] Integrate wiki request with server

--- server/controllers/wiki.js
+++ server/controllers/wiki.js
@@ -1,12 +1,20 @@
+var express    = require('express');
+var router = express.Router();
 var request = require('request');
+module.exports = router;
 
-function getWikiPage(topic) {
+router.get('/:topic', function(req, res) {
+  getWikiPage(req.params.topic, function(data) {
+    res.send(data);
+  });
+});
+
+function getWikiPage(topic, cb) {
   var endpoint = 'https://en.wikipedia.org/w/api.php?';
   var reqParam = 'action=parse&format=json&page=' + encodeURI(topic) + '&prop=text';
   var reqUrl = endpoint + reqParam;
   request(reqUrl, function(error, response, body) {
-    console.log(JSON.parse(body).parse.text['*']);
+    cb(JSON.parse(body).parse.text['*']);
   });
 }
 
-getWikiPage('San Francisco');
