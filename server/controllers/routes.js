4af946e1ce71558b5dc819d5fb0577bc643ee87a
horologix <tangelocat@gmail.com>
2015-08-05 16:16:26 -0700

    [style] formatting

--- server/controllers/routes.js
+++ server/controllers/routes.js
@@ -1,13 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
-
 var wikiServices = require('./wikiServices');
-// var googleServices = require('./googleServices');
 
 // ---------------------------------------------------------------------------
 // ROUTES
 router.use('/wikiServices', wikiServices);
-// router.use('/googleServices', otherServices); 
 
 module.exports = router;
