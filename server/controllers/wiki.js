59f1377dbb617ff5f81d1d34362efe18655eee0d
carterchung <carterchung@gmail.com>
2015-08-05 16:41:45 -0700

    [fix] Further parse wiki response

--- server/controllers/wiki.js
+++ server/controllers/wiki.js
@@ -1,12 +1,12 @@
 var request = require('request');
 
 function getWikiPage(topic) {
-  var endpoint = 'https://en.wikipedia.org/w/api.php?'
+  var endpoint = 'https://en.wikipedia.org/w/api.php?';
   var reqParam = 'action=parse&format=json&page=' + encodeURI(topic) + '&prop=text';
   var reqUrl = endpoint + reqParam;
   request(reqUrl, function(error, response, body) {
-    console.log(JSON.parse(body));
-  })
+    console.log(JSON.parse(body).parse.text['*']);
+  });
 }
 
 getWikiPage('San Francisco');
