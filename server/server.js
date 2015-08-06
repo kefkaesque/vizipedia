4cd373d1adb2e75a0849f51a6d77bd8d2d33e9bf
horologix <tangelocat@gmail.com>
2015-08-06 12:25:37 -0700

    [feat] base passport setup

--- server/server.js
+++ server/server.js
@@ -1,15 +1,29 @@
-var express    = require('express');
-var bodyParser = require('body-parser');
-var app        = express();
+var express = require('express');
+var passport = require('passport');
 
-var configEnv = require("./config/env.js");
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+var app = express();
+
+var configEnv = require('./config/env.js');
+var configPassport = require('./config/passport.js');
 var routes = require("./controllers/routes.js");
 
 // --------------------------------------------------------------------------------
 
-// configure body parser
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(session({
+  secret: 'Ark, don\'t eat that pie.',
+  resave: true,
+  saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+// --------------------------------------------------------------------------------
 
 app.use(express.static("public"));
 
