5eb048cd88efc8df8f80193df24a28fb8191d529
carterchung <carterchung@gmail.com>
2015-08-07 16:21:28 -0700

    [fix] Resolve merge conflict in package.json

---
+++ tests/databaseTests.js
@@ -0,0 +1,23 @@
+var chai = require('chai');
+var expect = chai.expect;
+
+
+describe('Database Schema', function() {
+  it('should be a valid test', function(){
+    expect(true).to.equal(true);
+  });
+
+  it('should be also be a valid test', function(){
+    expect(true).to.equal(true);
+  });
+});
+
+describe('login method', function() {
+  it('should be a valid test', function(){
+    expect(true).to.equal(true);
+  });
+
+  it('should be also be a valid test', function(){
+    expect(true).to.equal(true);
+  });
+});
