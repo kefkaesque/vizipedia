31b21cf65bd8b4a5d038963ca2782bebd5558f07
carterchung <carterchung@gmail.com>
2015-08-07 16:29:36 -0700

    [fix] Update test to work for new signup, login structure

--- tests/databaseTests.js
+++ tests/databaseTests.js
@@ -1,7 +1,7 @@
 var chai = require('chai');
 var expect = chai.expect;
 var Sequelize = require('sequelize');
-var Users = require('../db/users.js');
+var Users = require('../server/models/user.js');
 
 
 // describe('Database Schema', function() {
@@ -11,7 +11,7 @@ var Users = require('../db/users.js');
 
 // });
 
-describe('signupUser method', function() {
+describe('signup method', function() {
 
   var testUser = {
     username: 'testUser',
@@ -27,16 +27,24 @@ describe('signupUser method', function() {
     });
   })
 
+  after('remove test user from database', function() {
+    Users.destroy({
+      where: {
+        username: testUser.username
+      }
+    });
+  })
+
   it('should be a function', function(){
-    expect(Users.signupUser).to.be.a('function');
+    expect(Users.signup).to.be.a('function');
   });
 
   it('should return false if username already exists', function(done){
     //sign up our test user
-    Users.signupUser(testUser.username, testUser.password, testUser.email)
+    Users.signup(testUser.username, testUser.password, testUser.email)
     .then(function(){
       //sign up our test user again
-      return Users.signupUser(testUser.username, '', '');
+      return Users.signup(testUser.username, '', '');
     })
     .then(function(result){
       expect(result).to.equal(false);
@@ -47,10 +55,10 @@ describe('signupUser method', function() {
 
   it('should return false if username already exists', function(done){
     //sign up our test user
-    Users.signupUser(testUser.username, testUser.password, testUser.email)
+    Users.signup(testUser.username, testUser.password, testUser.email)
     .then(function(){
       //sign up a user with a different username but same email
-      return Users.signupUser('somethingElse', '', testUser.email);
+      return Users.signup('somethingElse', '', testUser.email);
     })
     .then(function(result){
       expect(result).to.equal(false);
@@ -60,7 +68,7 @@ describe('signupUser method', function() {
   });
 
   it('should return a new User instance if user and email are not in system', function(done){
-    Users.signupUser(testUser.username, testUser.password, testUser.email)
+    Users.signup(testUser.username, testUser.password, testUser.email)
     .then(function(user){
       expect(user.username).to.equal(testUser.username);
       done();
