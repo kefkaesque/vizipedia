c9fea776eb3bbf2d05565b7dfefd7ea82db12d12
horologix <tangelocat@gmail.com>
2015-08-07 13:03:35 -0700

    [fix] rewrite database

--- server/models/user.js
+++ server/models/user.js
@@ -1,74 +1,84 @@
 var Sequelize = require('sequelize');
-var db = require('../server/config/postgres.js');
+var db = require('../config/postgres.js');
 
-var Users = db.define('users', 
-  {
-    username: {
-      type: Sequelize.STRING
-    },
-    email: {
-      type: Sequelize.STRING
-    },
-    password: {
-      type: Sequelize.STRING
-    }
+var schema = {
+  username: {
+    type: Sequelize.STRING
   },
-  {
-    classMethods: {
-      signupUser: function(username, password, email) {
-        // If user name found, throw warning. Then if email found, throw warning. Else create new user account
-        return this.findOne({ 
-          where: {
-            username: username
-          } 
-        })
-        .then(function(result) {
-          console.log('result: ', result)
-          if (result) {
-            console.log('Username exists!');
-            return false;
-          } else {
-            return this.findOne({ 
-              where: {
-                email: email
-              } 
-            })
-            .then(function(result) {
-              if (result) {
-                console.log('Email exists!');
-                return false;
-              } else {
-                return this.create({username: username, password: password, email: email})
-                .then(function(user){
-                  return user;
-                });
-              }
-            })
-          }
-        });
-      },
-      login: function(username, pw) {
-        return this.findOne({
-          where: {
-            username: username
-          }
-        })
-        .then(function(result) {
-          if(result.password === pw){
-            console.log('You are logged in!');
-            return result;
-          }else{
-            console.log('Your password is incorrect!');
-            return false;
-          }
-        });
-      }
-    }
+  email: {
+    type: Sequelize.STRING
+  },
+  password: {
+    type: Sequelize.STRING
   }
-);
+};
 
-/* .sync() makes Sequelize create the database table for us if it doesn't
- *  exist already: */
+var classMethods = {};
+
+classMethods.login = function(username, password) {
+  return User.findOne({
+    where: {
+      username: username
+    }
+  })
+  .then(function(user) {
+    if(user.usesPassword(password)) {
+      return user;
+    }
+    return false;
+  });
+};
+
+classMethods.signup = function(username, password, email) {
+  return User.findOne({
+    where: {
+      username: username
+    }
+  })
+  .then(function(user) {
+    if(user) {
+      return user;
+    }
+    return User.findOne({
+      where: {
+        username: username
+      }
+    });
+  })
+  .then(function(user) {
+    if(user) {
+      return false;
+    }
+    return User.create({username: username, password: password, email: email})
+    .then(function(user){
+      return user;
+    });
+  });
+}
+
+// --------------------------------------------------------------------------------
+
+var instanceMethods = {};
+
+instanceMethods.usesPassword = function(password) {
+  return this.password === password;
+};
+
+// --------------------------------------------------------------------------------
+
+function isValidUsername(username) {
+  return true;
+}
+
+function isValidPassword(password) {
+  return true;
+}
+
+function isValidEmail(email) {
+  return true;
+}
+
+var User = db.define('user', schema, {classMethods: classMethods, instanceMethods: instanceMethods});
+
 db.sync();
-
-module.exports = Users;
+module.exports = User;
--- server/models/wikiArticle.js
+++ server/models/wikiArticle.js
@@ -1,11 +1,11 @@
 var Sequelize = require('sequelize');
 var db = require('../config/postgres.js');
 
-var wikiArticle = sequelize.define('wikiarticle', {
-  page_title: {
+var wikiArticle = db.define('wikiarticle', {
+  title: {
     type: Sequelize.STRING
   },
-  page_content: {
+  content: {
     type: Sequelize.STRING
   }
 });
