786e28c3a74b33f0347f1908dd1d510fde0c1b2d
pbbyeung <pbbyeung@gmail.com>
2015-08-11 11:26:09 -0700

    [fix] ?

--- worker/worker.js
+++ worker/worker.js
@@ -44,6 +44,7 @@ amqp.connect(url).then(function(conn) {
           article = Vizifier.vizify(article, title);
         else
           article = '';
+        console.log("******", query, title, article.substring(0,100));
         WikiArticle.create({query: query, title: ntitle, content: article});
         /*if(query !== ntitle) {
           WikiArticle.create({query: query, title: ntitle, content: ''});
