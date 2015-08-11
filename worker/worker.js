01670a1c6e995643e4c7a66bc0b472a6d769e134
pbbyeung <pbbyeung@gmail.com>
2015-08-11 11:29:21 -0700

    [fix] ?

--- worker/worker.js
+++ worker/worker.js
@@ -45,7 +45,7 @@ amqp.connect(url).then(function(conn) {
         else
           article = '';
         console.log("******", query, title, article.substring(0,100));
-        WikiArticle.create({query: query, title: ntitle, content: article});
+        //WikiArticle.create({query: query, title: ntitle, content: article});
         /*if(query !== ntitle) {
           WikiArticle.create({query: query, title: ntitle, content: ''});
           if(query !== title)
