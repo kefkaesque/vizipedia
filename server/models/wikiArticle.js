bcdeac73969f38f0523137cd7fdfe0b64818d87a
pbbyeung <pbbyeung@gmail.com>
2015-08-11 11:17:00 -0700

    [fix] ?

--- server/models/wikiArticle.js
+++ server/models/wikiArticle.js
@@ -10,7 +10,7 @@ var wikiArticle = db.define('wikiarticle', {
     type: Sequelize.STRING
   },
   content: {
-    type: Sequelize.STRING
+    type: Sequelize.TEXT
   },
   recommend: {
     type: Sequelize.INTEGER
