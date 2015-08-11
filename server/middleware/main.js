d6234002b3e233cf4101d784b38be49b3142803e
horologix <tangelocat@gmail.com>
2015-08-11 15:42:40 -0700

    [feat] update middleware

--- server/middleware/main.js
+++ server/middleware/main.js
@@ -2,15 +2,15 @@ var express = require('express');
 var app = express.Router();
 module.exports = app;
 
-// Set up local user variables
-app.use(function(req, res, next) {
+var userMiddleware = require('./user');
+
+function init(req, res, next) {
   res.locals.user = {};
   res.locals.Locals = {};
-  if(req.user) {
-    res.locals.username = req.user.username;
-    res.locals.user.username = req.user.username;
-    res.locals.user.id = req.user.id;
-    res.locals.Locals.userid = req.user.id;
-  }
   next();
-});
+}
+
+app.use(
+  init,
+  userMiddleware
+);
---
+++ server/middleware/user.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express.Router();
+module.exports = app;
+
+// Set up local user variables
+app.use(function(req, res, next) {
+  if(req.user) {
+    res.locals.user.username = req.user.username;
+    res.locals.user.id = req.user.id;
+    res.locals.Locals.userid = req.user.id;
+  }
+  next();
+});
