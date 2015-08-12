765332b9d8259e3dde4f8e8071f746e17c13c32a
carterchung <carterchung@gmail.com>
2015-08-12 10:59:51 -0700

    [feat] Stage comments for integration with database

--- server/routes/comments.js
+++ server/routes/comments.js
@@ -1,19 +1,29 @@
 var express = require('express');
 var router = express.Router();
+// var commentModel = require('../models/comment.js');
 module.exports = router;
 
+/* dummy data */
 var data = [
   {author: "Carter", text: "Hey guys"},
   {author: "David", text: "How's it going?"},
   {author: "Patrick", text: "Not bad, making good progress."},
   {author: "Stephen", text: "Good work!"}
 ];
+/* end dummy data */
 
-router.get('/test', function(req, res) {
-  res.send(JSON.stringify(data));
+router.get('/:articleId', function(req, res) {
+  //commentModel.wikiarticlegetcomment(req.params.articleId);
+  res.send(JSON.stringify(data)); /* dummy data */
 });
 
-router.post('/test', function(req, res) {
-  data.push(req.body);
-  res.send(JSON.stringify(data));
+router.post('/:articleId', function(req, res) {
+  console.log("article id: ", req.params.articleId);
+  console.log("comment:", req.body);
+  if (res.locals.user.id) {
+    console.log("user id: ", res.locals.user.id)
+  }
+  //commentModel.addcomment(req.body.text, res.locals.user.id, req.params.articleId);
+  data.push(req.body); /* dummy data */
+  res.send(JSON.stringify(data)); /* dummy data */
 });
