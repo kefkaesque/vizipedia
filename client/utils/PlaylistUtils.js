65014009e0928e0bc498b4edc4d73535ae7113dd
carterchung <carterchung@gmail.com>
2015-08-15 15:45:01 -0700

    [feat] Add playlist store

--- client/utils/PlaylistUtils.js
+++ client/utils/PlaylistUtils.js
@@ -6,6 +6,7 @@ module.exports = {
   createPlaylist: function(playlistName) {
     console.log('PlaylistUtils.js: creating playlist');
     //send post request here
-    // PlaylistActions.dispatchPlaylistCreation(playlistId);
+    var playlistId = 1337; //dummy data
+    PlaylistActions.dispatchPlaylistCreation(playlistId);
   }
 };
