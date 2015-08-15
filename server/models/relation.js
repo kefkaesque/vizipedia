66d4a111808f6e92ec01c61b1b1ee04171f60130
pbbyeung <pbbyeung@gmail.com>
2015-08-15 15:05:17 -0700

    [feat] create new model for following button

---
+++ server/models/relation.js
@@ -0,0 +1,84 @@
+var Sequelize = require('sequelize');
+var db = require('../config/postgres.js');
+var VisitedArticle = require('./VisitedArticle.js');
+
+var schema = {
+  follower: {
+    type: Sequelize.INTEGER
+  },
+  following: {
+    type: Sequelize.INTEGER
+  },
+};
+
+var classMethods = {};
+// run if isFollowing is False
+classMethods.follow = function(userId, followingId) {
+  return Relation.findOrCreate({
+    where: {
+      follower: userId,
+      following: followingId
+    }
+  });
+};
+// run if isFollowing method is true
+classMethods.unFollow = function(userId, followingId) {
+  return Relation.destroy({
+    where: {
+      follower: userId,
+      following: followingId
+    }
+  });
+};
+// return true or false if the user is following
+classMethods.isFollowing = function(userId, followingId) {
+  return Relation.findOne({
+    where: {
+      follower: userId,
+      following: followingId
+    }
+  })
+  .then(function(entry) {
+    if (entry) {
+      return true;
+    }
+    return false;
+  });
+};
+// get all recommended articles from all following
+classMethods.getFollowingRecommended = function(userId) {
+  return Relation.findAll({
+    where: {
+      follower: userId,
+    }
+  })
+  .then(function(following) {
+    var ids = [];
+    for (var i = 0; i < following.length; i++) {
+      ids.push(following[i].dataValues.following);
+    }
+    return ids;
+  })
+  .then(function(res) {
+    return VisitedArticle.findAll({
+      where: {
+        userId: res,
+        recommended: true
+      }
+    })
+    .then(function(recs) {
+      var articleIds = [];
+      for (var j = 0; j < recs.length; j++) {
+        articleIds.push(recs[j].dataValues.articleId);
+      }
+      console.log(articleIds);
+      return articleIds;
+    });
+  });
+};
+
+var Relation = db.define('relations', schema, {classMethods: classMethods});
+
+db.sync();
+module.exports = Relation;
+
