025d7f1db725b9e55cdc23a50c1b30f4dd6f7ae3
horologix <tangelocat@gmail.com>
2015-08-18 12:27:19 -0700

    [fix] rename, get user playlist logic

--- server/models/Playlist.js
+++ server/models/Playlist.js
@@ -2,7 +2,7 @@ var Sequelize = require('sequelize');
 var db = require('../config/postgres');
 
 var schema = {
-  user_id: {type: Sequelize.INTEGER},
+  userId: {type: Sequelize.INTEGER},
   name: {type: Sequelize.STRING}
 };
 
