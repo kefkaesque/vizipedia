8ec28d5dca98b4cb9bd17a475948920500795b1c
pbbyeung <pbbyeung@gmail.com>
2015-08-19 22:14:31 -0700

    [fix] merge

--- client/components/Recommend.react.js
+++ client/components/Recommend.react.js
@@ -1,31 +1,24 @@
 var React = require('react');
-var RecUtils = require('../utils/RecUtils');
-var ArticleStore = require('../stores/ArticleStore');
-
-function getRecommendState() {
-  console.log('getting recommended state');
-  return {
-    data: ArticleStore.getData(),
-  };
-}
+var RecommendStore = require('../stores/RecommendStore');
+var RecActions = require('../actions/RecActions');
 
 var RecommendButton = React.createClass({
   getInitialState: function() {
-    return getRecommendState();
+    return {};
   },
   componentDidMount: function() {
-    ArticleStore.addChangeListener(this._onChange);
+    RecommendStore.addChangeListener(this._onChange);
   },
   componentWillUnmount: function() {
-    ArticleStore.removeChangeListener(this._onChange);
+    RecommendStore.removeChangeListener(this._onChange);
   },
-  handlePress: function(articleId) {
-    RecUtils.getRecData(articleId);
+  handlePress: function() {
+    RecActions.dispatchRec();
   },
   render: function() {
     return (
       <div className="item">
-        <button className="recommend" onClick={this.handlePress.bind(this, this.state.data.id)}>
+        <button className="recommend" onClick={this.handlePress}>
         Recommend {this.state.data.recs}
         </button>
         <span className="rCount"></span>
@@ -33,8 +26,58 @@ var RecommendButton = React.createClass({
     );
   },
   _onChange: function() {
-    this.setState(getRecommendState());
+    this.setState(RecommendStore.getData());
   }
 });
 
 module.exports = RecommendButton;
+
+
+//   getInitialState: function() {
+//     return {data: '', loaded: false};
+//   },
+//   componentWillMount: function() {
+//     ArticleStore.addChangeListener(this._onChange);
+//   },
+//   componentDidMount: function() {
+//     var query = window.location.pathname.split('/')[2];
+//     ArticleActions.dispatchArticle(query);
+//   },
+//   componentWillReceiveProps: function() {
+//     this.setState({
+//       data: '',
+//       loaded: false
+//     });
+//     var query = window.location.pathname.split('/')[2];
+//     ArticleActions.dispatchArticle(query);
+//   },
+//   componentWillUnmount: function() {
+//     ArticleStore.removeChangeListener(this._onChange);
+//   },
+//   createMarkup: function() {
+//     return {__html: this.state.data.content};
+//   },
+//   render: function() {
+//     return (
+//       <div className="filled">
+//         <Loader loaded={this.state.loaded} lines={13} length={30} width={15} radius={80}
+//         corners={1} rotate={0} direction={1} color="#000" speed={1}
+//         trail={60} shadow={false} hwaccel={false} className="spinner"
+//         zIndex={2e9} top="50%" left="50%" scale={1.00} >
+//           <div className="wrapper article serif">
+//             <Recommend articleId={this.state.data.id} />
+//           <div dangerouslySetInnerHTML={this.createMarkup()} />
+//           </div>
+//         </Loader>
+//       </div>
+//     );
+//   },
+//   _onChange: function() {
+//     this.setState({
+//       data: ArticleStore.getData(),
+//       loaded: true
+//     });
+//   }
+// });
+
+// module.exports = Article;
