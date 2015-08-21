d17486d4cc03728cf0ab13356f4f84ea56cf9837
pbbyeung <pbbyeung@gmail.com>
2015-08-21 14:34:32 -0700

    [refactor] clean up files

---
+++ client/components/Loader.react.js
@@ -0,0 +1,12 @@
+var React = require('react');
+
+var Loader = React.createClass({
+
+  render: function() {
+    return (
+      <div> Loader goes here </div>
+    );
+  }
+}
+
+module.exports = Loader;
