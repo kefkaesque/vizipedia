8f7baefa96109819cb48b77a22372e0312b6648d
carterchung <carterchung@gmail.com>
2015-08-22 13:19:36 -0700

    [feat] pseudocode router for race

--- server/api/race.js
+++ server/api/race.js
@@ -5,10 +5,35 @@ module.exports = router;
 //Require the associated models (Race, Racer)
 
 router.post('/', function(req, res) {
-  if()
+
+  if(req.body.raceId) {
+  // If race exists, indicate that user has finished
+    // incoming: raceId, finishTime, path (array of articleId)
+    // user id stored in locals
+
+    // set finishTime and path of racer model corresponding to user id
+    // get racer models of everyone who is done
+
+    // send back array of objects of everyone who is done (racer models)
+    // each object has a username, path (array of article topics), and finishTime
+  } else {
+  // Create a race
+    // incoming: startTopic, endTopic, users (array of usernames)
+    // user id stored in locals
+
+    // create new race model with startTopic, endTopic
+    // then, for each user, including logged in user, create new racer models with raceId from above
+    // need to get user ids from usernames
+    // finish time: null, path: null
+
+    // send back raceId, startId, endId
+  }
 });
 
 router.get('/:raceId', function(req, res) {
+  var raceId = req.query.raceId;
+  // get racer models of everyone who is done corresponding to raceId
 
+  // send back array of objects of everyone who is done (racer models)
 });
 // --------------------------------------------------------------------------------
