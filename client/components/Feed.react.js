5cbb6589ab99dfff3f427ca7246d0ef8c0aa7835
ying-hung Lai <yinghunglai@ying-hungs-MacBook-Air.local>
2015-08-22 17:24:01 -0700

    [fix] use recommend table instead of visited table

--- client/components/Feed.react.js
+++ client/components/Feed.react.js
@@ -1,36 +1,36 @@
-// var React = require('react');
-// var FeedStore = require('../stores/FeedStore');
+var React = require('react');
+var FeedStore = require('../stores/FeedStore');
 // var FeedUtils = require('../utils/FeedUtils');
-// var Router = require('react-router');
+var Router = require('react-router');
 
-// function getFeedState() {
-//   return {
-//     data: FeedStore.getData(),
-//   };
-// }
+function getFeedState() {
+  return {
+    data: FeedStore.getData(),
+  };
+}
 
-// var Feed = React.createClass({
-//   getInitialState: function() {
-//     FeedUtils.getFeedData();
-//     return getFeedState();
-//   },
-//   componentDidMount: function() {
-//     FeedStore.addChangeListener(this._onChange);
-//   },
-//   componentWillUnmount: function() {
-//     FeedStore.removeChangeListener(this._onChange);
-//   },
-//   render: function() {
-//     return (
-//       <div>
-//         <h1> FEED DATA </h1>
-//         <div> {this.state.data} </div>
-//       </div>
-//     );
-//   },
-//   _onChange: function() {
-//     this.setState(getFeedState());
-//   }
-// });
+var Feed = React.createClass({
+  getInitialState: function() {
+    // FeedUtils.getFeedData();
+    // return getFeedState();
+  },
+  componentDidMount: function() {
+    FeedStore.addChangeListener(this._onChange);
+  },
+  componentWillUnmount: function() {
+    FeedStore.removeChangeListener(this._onChange);
+  },
+  render: function() {
+    return (
+      <div>
+        <h1> FEED DATA </h1>
+        <div> {this.state.data} </div>
+      </div>
+    );
+  },
+  _onChange: function() {
+    this.setState(getFeedState());
+  }
+});
 
-// module.exports = Feed;
+module.exports = Feed;
