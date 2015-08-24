809564ca3af9657a101ca73a6113489ded366e34
pbbyeung <pbbyeung@gmail.com>
2015-08-24 16:38:26 -0700

    [fix] new haiku

--- client/components/404.react.js
+++ client/components/404.react.js
@@ -8,9 +8,9 @@ var Haiku = React.createClass({
       {this.props.user.data.following !== undefined ? this.props.children :
         <div>
           <h2> {this.props.user.data.username} not found, a Haiku </h2>
-          <div> our <a href='/'>search</a> is lonely, </div>
-          <div> with searching comes loss, </div>
-          <div> and the presence of absence. </div>
+          <div> our <a href='/'>search</a> is empty, </div>
+          <div> lost amongst a morning fog, </div>
+          <div> awaiting the sun. </div>
         </div>
       }
       </div>
