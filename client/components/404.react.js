1d0dfff7b7d86b04457b914d412dfdfb6535f443
pbbyeung <pbbyeung@gmail.com>
2015-08-24 17:53:50 -0700

    [feat] bad articles go to 404

--- client/components/404.react.js
+++ client/components/404.react.js
@@ -3,11 +3,12 @@ var React = require('react');
 var Haiku = React.createClass({
 
   render: function() {
+    console.log('this haiku props ,', this.props);
     return (
       <div className="filled">
-      {this.props.user.data.following !== undefined ? this.props.children :
+      { this.props.user.data.following !== undefined || this.props.user.data.content !== undefined ? this.props.children :
         <div>
-          <h2> {this.props.user.data.username} not found, a Haiku </h2>
+          <h2> {this.props.user.data.username || this.props.user.data.title} not found, a Haiku </h2>
           <div> our <a href='/'>search</a> is empty, </div>
           <div> lost amongst a morning fog, </div>
           <div> awaiting the sun. </div>
