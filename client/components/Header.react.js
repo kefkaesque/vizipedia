d536c416234d82578d4882a1458a5a3cadce8c75
pbbyeung <pbbyeung@gmail.com>
2015-08-25 23:32:39 -0700

    [refactor] remove repeated code and use search component

--- client/components/Header.react.js
+++ client/components/Header.react.js
@@ -2,6 +2,7 @@ var React = require('react');
 var Router = require('react-router');
 var Link = Router.Link;
 var RaceActions = require('../actions/RaceActions');
+var Search = require('./Search.react');
 
 var Header = React.createClass({
   render: function() {
@@ -13,8 +14,7 @@ var Header = React.createClass({
           <FeedButton/>
         </div>
         <Link to="landing"><div className="logo serif">vizipedia</div></Link>
-        <HeaderForm />
-        <UserSearch />
+        <Search />
       </div></div>
     )
   }
@@ -59,64 +59,6 @@ var FeedButton = React.createClass({
           Feed
         </button>
       </div>
-    );
-  }
-});
-
-var HeaderForm = React.createClass({
-  mixins: [ Router.Navigation ],
-
-  handleSubmit: function(e) {
-    e.preventDefault();
-    RaceActions.cancelRace();
-    var text = React.findDOMNode(this.refs.text).value.trim();
-    if (!text) {
-      return;
-    }
-    if (text.substr(0,5) === 'user:') {
-      text = text.substr(5).trim();
-      if (!text) {
-        return;
-      }
-      this.transitionTo('profile', {username: text});
-    } else {
-      this.transitionTo('wiki', {topic: text});
-    }
-    React.findDOMNode(this.refs.text).value = '';
-    return;
-  },
-  render: function() {
-    return (
-      <form className="headerForm" onSubmit={this.handleSubmit}>
-        <input type="text" placeholder="Search..." ref="text" />
-        <button type="submit"><span className="fa fa-fw fa-search"></span></button>
-      </form>
-    );
-  }
-});
-
-
-var UserSearch = React.createClass({
-  mixins: [ Router.Navigation ],
-
-  handleSubmit: function(e) {
-    e.preventDefault();
-    RaceActions.cancelRace();
-    var text = React.findDOMNode(this.refs.text).value.trim();
-    if (!text) {
-      return;
-    }
-    this.transitionTo('profile', {username: text});
-    React.findDOMNode(this.refs.text).value = '';
-    return;
-  },
-
-  render: function() {
-    return (
-      <form className="headerForm" onSubmit={this.handleSubmit}>
-        <input type="text" placeholder="Search Users" ref="text" />
-        <button type="submit"><span className="fa fa-search"></span></button>
-      </form>
     );
   }
 });
--- client/components/Landing.react.js
+++ client/components/Landing.react.js
@@ -1,5 +1,5 @@
 var React = require('react');
-var Router = require('react-router');
+var Search = require('./Search.react');
 
 var Landing = React.createClass({
   render: function() {
@@ -8,7 +8,7 @@ var Landing = React.createClass({
         <div className="spacer"></div>
         <div>
           <div className="logo serif">vizipedia</div>
-          <SearchBar />
+          <Search />
         </div>
       </div>
     )
@@ -16,33 +16,3 @@ var Landing = React.createClass({
 });
 
 module.exports = Landing;
-
-
-var SearchBar = React.createClass({
-  mixins: [ Router.Navigation ],
-
-  handleSubmit: function(e) {
-    e.preventDefault();
-    var text = React.findDOMNode(this.refs.text).value.trim();
-    if (!text) {
-      return;
-    }
-    if (text.substr(0,5) === 'user:') {
-      text = text.substr(5).trim();
-      if (!text) {
-        return;
-      }
-      this.transitionTo('profile', {username: text});
-    } else {
-      this.transitionTo('wiki', {topic: text});
-    }
-  },
-  render: function() {
-    return (
-      <form className="headerForm" onSubmit={this.handleSubmit}>
-        <input type="text" placeholder="Search..." ref="text" />
-        <button type="submit"><span className="fa fa-fw fa-search"></span></button>
-      </form>
-    );
-  }
-});
