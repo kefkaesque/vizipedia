9a7ae212a0c5104a151d20c140213ec2fded686f
pbbyeung <pbbyeung@gmail.com>
2015-08-25 23:32:23 -0700

    [feat] new search component

---
+++ client/components/Search.react.js
@@ -0,0 +1,34 @@
+var React = require('react');
+var Router = require('react-router');
+
+var Search = React.createClass({
+  mixins: [ Router.Navigation ],
+
+  handleSubmit: function(e) {
+    e.preventDefault();
+    var text = React.findDOMNode(this.refs.text).value.trim();
+    if (!text) {
+      return;
+    }
+    if (text.substr(0,5) === 'user:') {
+      text = text.substr(5).trim();
+      if (!text) {
+        return;
+      }
+      this.transitionTo('profile', {username: text});
+    } else {
+      this.transitionTo('wiki', {topic: text});
+    }
+    React.findDOMNode(this.refs.text).value = '';
+  },
+  render: function() {
+    return (
+      <form className="headerForm" onSubmit={this.handleSubmit}>
+        <input type="text" placeholder="Search..." ref="text" />
+        <button type="submit"><span className="fa fa-fw fa-search"></span></button>
+      </form>
+    );
+  }
+});
+
+module.exports = Search;
